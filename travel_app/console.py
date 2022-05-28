ce6ee02203496b685f126fd1a2360118da463554
CodeClan Student <codeclanstudent@MacBook-Pro-2.local>
2022-05-28 15:21:55 +0100

    Populated Cities Table, Connection to database and saving with foreign keys successful

--- travel_app/console.py
+++ travel_app/console.py
@@ -3,12 +3,18 @@ from models.city import City
 from models.country import Country
 
 import repositories.country_repository as country_repository
+import repositories.city_repository as city_repository
 
 country_repository.delete_all()
+city_repository.delete_all()
 
 country1 = Country("Spain", False)
 country_repository.save(country1)
 country2 = Country("France", True)
 country_repository.save(country2)
 
+city1 = City ("Barcelona", False, country1)
+city_repository.save(city1)
+city2 = City("Paris", True, country2)
+city_repository.save(city2)
 
