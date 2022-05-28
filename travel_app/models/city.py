a527c8652f020138ed9f0c022b56e325e6b20e82
CodeClan Student <codeclanstudent@MacBook-Pro-2.local>
2022-05-28 17:08:09 +0100

    Changed default value for visited to False for new entries of cities and countries

--- travel_app/models/city.py
+++ travel_app/models/city.py
@@ -1,6 +1,9 @@
 class City:
-    def __init__(self, name, visited, country, id=None):
+    def __init__(self, name, country, visited= False, id=None):
         self.name = name
         self.visited = visited
         self.country = country
         self.id=id
+
+    def mark_visited(self):
+        self.visited = True
--- travel_app/models/country.py
+++ travel_app/models/country.py
@@ -1,6 +1,9 @@
 class Country:
 
-    def __init__(self, name, visited, id=None):
+    def __init__(self, name, visited=False, id=None):
         self.name = name
         self.visited = visited
         self.id = id
+
+    def mark_visited(self):
+        self.visited = True
