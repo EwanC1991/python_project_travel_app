323c8b68c7f42316dc13d29447e45d86ae50b570
CodeClan Student <codeclanstudent@MacBook-Pro-2.local>
2022-05-29 16:42:24 +0100

    Added create route on city_controller

--- travel_app/controllers/city_controller.py
+++ travel_app/controllers/city_controller.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect
 from flask import Blueprint
+from models.city import City
 from models.country import Country
 import repositories.city_repository as city_repository
 import repositories.country_repository as country_repository
@@ -13,9 +14,20 @@ def cities():
 
 # NEW
 # GET 'cities/new'
+@cities_blueprint.route("/cities/new", methods=['GET'])
+def new_city():
+    countries = country_repository.select_all()
+    return render_template("/cities/new.html", all_countries=countries)
 
 # CREATE
 # POST '/cities'
+@cities_blueprint.route("/cities", methods=['POST'])
+def create_city():
+    name = request.form['name']
+    country = country_repository.select(request.form['country_id'])
+    city = City(name, country)
+    city_repository.save(city)
+    return redirect('/cities')
 
 # SHOW
 # GET '/cities/<id>'
