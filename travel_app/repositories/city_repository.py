e5cc8602aa0349be81a377396eb671d91bbfec3d
CodeClan Student <codeclanstudent@MacBook-Pro-2.local>
2022-06-01 11:08:08 +0100

    Got a pseudo code example from my project for PDA and took a picture of my Wireframes

--- travel_app/repositories/city_repository.py
+++ travel_app/repositories/city_repository.py
@@ -6,11 +6,17 @@ from models.sights import Sight
 import repositories.country_repository as country_repository
 
 def save(city):
+    #TELLING SQL THAT I WOULD LIKE TO ENTER SOME VALUES INTO THE CITIES TABLE
     sql = "INSERT INTO cities (name, country_id, visited) VALUES (?, ?, ?) RETURNING *"
+    #PRESENTING THE VALUES I WOULD LIKE TO ENTER
     values = [city.name, city.country.id, city.visited]
+    #TAKE THE SQL AND VALUES AND ENTER THEM INTO THE RUN_SQL FUNCTION
     results = run_sql(sql, values)
+    #LOOK FOR THE ID ASSIGNED TO THE CITY ADDED
     id = results[0]['id']
+    #ADD THAT ID TO THE CITY ADDED
     city.id = id
+    #RETURN THE CITY WITH ITS NEW ID
     return city
 
 def select_all():
